cda23ae535e4b34a5999620caf1c4986d56c03ce
Jarkko Lagus <jarkko@iprally.com>
2022-10-09 00:58:31 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::f64::consts::PI;
+use num::complex::Complex;
+
+
+fn fft(xs: Vec<f64>) -> Vec<Complex<f64>> {
+    // Following the Python version from https://pythonnumericalmethods.berkeley.edu/notebooks/chapter24.03-Fast-Fourier-Transform.html
+    let n = xs.len();
+
+    if n == 1 {
+        return vec![Complex::new(xs[0], 0.0)]
+    }
+
+    let x_even: Vec<f64> = xs.iter().step_by(2).copied().collect();
+    let x_odd: Vec<f64> = xs.iter().skip(1).step_by(2).copied().collect();
+    
+    let x_even_res = fft(x_even);
+    let x_odd_res = fft(x_odd);
+
+    let factor_mul: Complex<f64> = Complex::new(0.0, -2.0 * PI);
+
+    let mut c_p1: Vec<Complex<f64>> = Vec::new();
+    let mut c_p2: Vec<Complex<f64>> = Vec::new();
+
+    for i in (0..(n/2)) {
+        c_p1.push(
+            x_even_res[i] + 
+            (factor_mul * Complex::new(i as f64, 0.0) / Complex::new(n as f64, 0.0)).exp() * 
+            x_odd_res[i]
+        );
+        c_p2.push(
+            x_even_res[i] + 
+            (factor_mul * Complex::new((i + (n / 2)) as f64, 0.0) / Complex::new(n as f64, 0.0)).exp() *
+            x_odd_res[i]
+        );
+    }    
+
+    return [c_p1, c_p2].concat();
+}
+
+fn main() {
+    let xs: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
+
+    let res = fft(xs);
+    println!("{:?}", res);
+}
