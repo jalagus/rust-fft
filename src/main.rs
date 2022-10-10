63b481bce205ac6d746e600e723b415840799666
Jarkko Lagus <jarkko@iprally.com>
2022-10-10 10:51:31 +0300

    Refactor to use iterators

--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,19 @@ fn fft(xs: Vec<f64>) -> Vec<Complex<f64>> {
     let x_even_res = fft(x_even);
     let x_odd_res = fft(x_odd);
 
-    let factor_mul: Complex<f64> = Complex::new(0.0, -2.0 * PI);
+    let factor_mul: Complex<f64> = Complex::new(0.0, -2.0 * PI) / Complex::new(n as f64, 0.0);
 
-    let mut c_p1: Vec<Complex<f64>> = Vec::new();
-    let mut c_p2: Vec<Complex<f64>> = Vec::new();
+    let c_p1: Vec<Complex<f64>> = (0..(n/2)).map(
+        |i| x_even_res[i] + 
+        (factor_mul * Complex::new(i as f64, 0.0)).exp() *
+        x_odd_res[i]
+    ).collect();
 
-    for i in (0..(n/2)) {
-        c_p1.push(
-            x_even_res[i] + 
-            (factor_mul * Complex::new(i as f64, 0.0) / Complex::new(n as f64, 0.0)).exp() * 
-            x_odd_res[i]
-        );
-        c_p2.push(
-            x_even_res[i] + 
-            (factor_mul * Complex::new((i + (n / 2)) as f64, 0.0) / Complex::new(n as f64, 0.0)).exp() *
-            x_odd_res[i]
-        );
-    }    
+    let c_p2: Vec<Complex<f64>> = (0..(n/2)).map(
+        |i| x_even_res[i] + 
+        (factor_mul * Complex::new((i + (n/2)) as f64, 0.0)).exp() *
+        x_odd_res[i]
+    ).collect();
 
     return [c_p1, c_p2].concat();
 }
@@ -41,5 +37,8 @@ fn main() {
     let xs: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
 
     let res = fft(xs);
-    println!("{:?}", res);
+
+    for item in res.iter() {
+        println!("{:>7.2} + {:>7.2}j", item.re, item.im);
+    }
 }
